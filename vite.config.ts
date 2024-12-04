ff1142daa1611a3520c4bfbfcee3146e901bd7fd
di-galion <dimitiy@Dimitiys-MacBook-Pro.local>
2024-12-04 19:50:22 +0200

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/platform/apps/online/',
+    base: '/platform/apps/games/',
     css: {
         preprocessorOptions: {
             scss: {
